a98b1a68322c8e29d6d82ba29b15fc1e9af7808d
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-11 13:27:14 -0800

    init commit

---
+++ src/index/index.js
@@ -0,0 +1,5 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+//load scripts
+//load videos
---
+++ webpack.config.js
@@ -0,0 +1,158 @@
+const webpack = require("webpack"); 
+const path = require("path"); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //transfer style vis css not js
+const HtmlWebpackPlugin = require('html-webpack-plugin') ; 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: 'development', //development | production
+    optimization: {
+        minimize: false //for minify .js files
+    },
+    entry: {
+        'index' : './src/index/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        //publicPath: './assets/imgs/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader" ,
+				exclude: /node_modules/,
+            },
+            { //for transfer .css via css files
+                test: /\.css$/,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        hmr: process.env.NODE_ENV === 'development',
+                        reloadAll: true,
+                      },
+                    },
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
+            // { //for transfer .css via js files
+            //     test: /\.css$/,
+            //     use: ['style-loader','css-loader','postcss-loader']
+            // },
+            {//for transfer .scss via css files
+                test:/\.scss$/,
+                use: [
+                    {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: {
+                        hmr: process.env.NODE_ENV === 'development',
+                        reloadAll: true,
+                      },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            // { //for transfer .scss via js files
+            //     test: /\.scss$/,
+            //     use: ['style-loader','css-loader','postcss-loader','sass-loader']
+            // },
+            {
+                test : /\.html$/,
+                exclude: /node_modules/,		
+                use: [ {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false , //for minify html or not
+                        publicPath: './'
+                    }
+                }]
+            },
+            {
+                test : /\.(png|jpg|jpeg)$/ ,
+                exclude: /node_modules/,
+                use : [ {
+                        loader : 'file-loader' ,
+                        options : {
+                            name : '[name].[ext]',
+                            outputPath : 'assets/imgs/',
+                            publicPath : 'assets/imgs/' ,
+                            esModule: false
+                        }
+                        }
+                ]
+            },
+            {
+                test : /\.(gif)$/ ,
+                exclude: /node_modules/,
+                use : [ {
+                        loader : 'file-loader' ,
+                        options : {
+                            name : '[name].[ext]',
+                            outputPath : 'assets/imgs/gifs',
+                            publicPath : 'assets/imgs/gifs',
+                            esModule: false
+                        }
+                        }
+                ]
+            },
+            {
+                test : /\.(ttf|otf|woff|woff2|eot)$/ ,
+                use : [ {
+                        loader : 'file-loader' ,
+                        options : {
+                            name : '[name].[ext]',
+                            outputPath : 'assets/fonts/',
+                            publicPath : 'assets/fonts/',
+                            esModule: false
+                        }
+                        }
+                ]
+            },
+            {
+                test : /\.svg$/ ,
+                use : [ {
+                        loader : 'file-loader' ,
+                        options : {
+                            name : '[name].[ext]',
+                            outputPath : 'assets/svgs/',
+                            publicPath : 'assets/svgs/',
+                            esModule: false
+                        }
+                        }
+                ]
+            },
+            {
+                test : /\.mp4$/ ,
+                use : [ {
+                        loader : 'file-loader' ,
+                        options : {
+                            name : '[name].[ext]',
+                            outputPath : 'assets/vids/',
+                            publicPath : 'assets/vids/',
+                            esModule: false
+                        }
+                        }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ 
+            filename: "[name].css", 
+            chunkFilename: '[id].css',
+            ignoreOrder: false 
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html' , 
+            inject: true,
+            chunks: ['index'],
+            template: './src/index/index.html' 
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
