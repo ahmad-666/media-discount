4666fb7acb480d70c4c8c48a501db556d9c4239c
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-15 15:51:20 -0800

    login full

---
+++ src/forgetPass/forgetPass.js
@@ -0,0 +1,8 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+import '../utilities/styles/form.scss'
+import '../init.scss' ;
+//load scripts
+import '../forgetPass/scripts/forgetPass.js' ;
+//load videos
