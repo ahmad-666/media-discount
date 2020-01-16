e73baf0372465dee12320423659c89ec1b3cbe56
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-16 14:56:41 -0800

    fix bookmark bug

--- src/medic/scripts/medicInfo.js
+++ src/medic/scripts/medicInfo.js
@@ -3,6 +3,15 @@ new ProductSlider(document.querySelector('.productSlider'),null) ;
 let bookmark = document.querySelector('.bookmark') ;
 let toast = bookmark.querySelector('.toast') ;
 bookmark.addEventListener('click',e=>{
-    toast.classList.add('show') ;
-    setTimeout(()=>toast.classList.remove('show'),3000)
+    bookmark.classList.toggle('trigger') ;
+    if(bookmark.classList.contains('trigger')){
+        toast.textContent = 'به لیست مورد علاقه ها اظافه شد'
+        toast.classList.add('show') ;
+        setTimeout(()=>toast.classList.remove('show'),1500)
+    }
+    else{
+        toast.textContent = 'از لیست مورد علاقه ها حذف شد'
+        toast.classList.add('show') ;
+        setTimeout(()=>toast.classList.remove('show'),1500)
+    }
 }) 
