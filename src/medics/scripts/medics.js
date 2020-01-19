e2de5adaae1a8a951181d261226a9d4ceb7e5013
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-19 15:31:03 -0800

    half dashborad resp

--- src/medics/scripts/medics.js
+++ src/medics/scripts/medics.js
@@ -32,3 +32,61 @@ filtersForm.querySelectorAll('label').forEach(label=>label.addEventListener('cli
 //filter handler------------------------------------
 let filterHandlerInstance = mainCategories.forEach(mainCategory=>new Filter.FilterHandler(currFiltersWrapper,mainCategory)) ;
 new Filter.CurrFilterClearAll(currFiltersWrapper,filtersForm,clearAllBtn,filterHandlerInstance) ;
+
+
+let mobileFilterSort = document.querySelector('#mobileFilterSort');
+let filters = document.querySelector('#filterWrapper') ;
+let sort = document.querySelector('#sortWrapper') ;
+let blackFilter = document.querySelector('#blackFilter') ;
+mobileFilterSort.querySelector('#mobileFilter').addEventListener('click',toggleMobileFilter) ;
+mobileFilterSort.querySelector('#mobileSort').addEventListener('click',toggleMobileSort) ;
+function toggleMobileFilter(e){
+    e.stopPropagation(e) ; 
+    if(!sort.classList.contains('show')){
+        blackFilter.classList.toggle('show') ;
+        document.body.classList.toggle('disableScroll') ;
+        filters.classList.toggle('show') ;
+        document.addEventListener('click',filterDoc) ; 
+    }
+    else{
+        sort.classList.remove('show') ;
+        document.removeEventListener('click',sortDoc) ;
+        filters.classList.toggle('show') ;
+        document.addEventListener('click',filterDoc) ;
+    }
+}
+function filterDoc(e){
+    e.stopPropagation(e) ;
+    let clickedElm = e.target; 
+    if(!filters.contains(clickedElm)){
+        blackFilter.classList.remove('show') ;
+        document.body.classList.remove('disableScroll') ;
+        filters.classList.remove('show') ;
+        document.removeEventListener('click',filterDoc) ;
+    }
+}
+function toggleMobileSort(e){
+    e.stopPropagation(e) ;
+    if(!filters.classList.contains('show')){
+        blackFilter.classList.toggle('show') ;
+        document.body.classList.toggle('disableScroll') ;
+        sort.classList.toggle('show') ;
+        document.addEventListener('click',sortDoc) ;
+    }
+    else{
+        filters.classList.remove('show') ;
+        document.removeEventListener('click',filterDoc) ;
+        sort.classList.toggle('show') ;
+        document.addEventListener('click',sortDoc) ;
+    }
+}
+function sortDoc(e){
+    e.stopPropagation(e) ;
+    let clickedElm = e.target; 
+    if(!sort.contains(clickedElm)){
+        blackFilter.classList.remove('show') ;
+        document.body.classList.remove('disableScroll') ;
+        sort.classList.remove('show') ;
+        document.removeEventListener('click',sortDoc) ;
+    }
+}
