875425edf4bf5388f1e4cc44dddeb8bb0841e2bf
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-25 16:32:46 -0800

    changes ver2

--- src/medic/scripts/medicMore.js
+++ src/medic/scripts/medicMore.js
@@ -1,2 +1,2 @@
-import Tabs from '../../utilities/scripts/tabs.js' ;
-document.querySelectorAll('.tabs_wrapper').forEach(tabWrapper =>new Tabs(tabWrapper));
+// import Tabs from '../../utilities/scripts/tabs.js' ;
+// document.querySelectorAll('.tabs_wrapper').forEach(tabWrapper =>new Tabs(tabWrapper));
