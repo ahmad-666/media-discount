adcccac30958b7425affeadbed8057abb0d29012
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-01-28 13:30:02 -0800

    final resp

--- src/medic/scripts/medicInfo.js
+++ src/medic/scripts/medicInfo.js
@@ -1,3 +1,5 @@
+import FontFaceObserver from 'fontfaceobserver' ;
+import Collapse from '../../utilities/scripts/collapse.js' ;
 import ProductSlider from '../../utilities/scripts/productSlider.js' ;
 new ProductSlider(document.querySelector('.productSlider'),null) ;
 let bookmark = document.querySelector('.bookmark') ;
@@ -15,8 +17,15 @@ bookmark.addEventListener('click',e=>{
         setTimeout(()=>toast.classList.remove('show'),1500)
     }
 }) 
-let shareTitle = document.querySelector('#medicInfo .right .share .title') ;
-let shareSocials =  document.querySelector('#medicInfo .right .share .socials') ;
-shareTitle.addEventListener('click',e=>{
-    shareSocials.classList.toggle('show') ;
+// let shareTitle = document.querySelector('#medicInfo .right .share .title') ;
+// let shareSocials =  document.querySelector('#medicInfo .right .share .socials') ;
+// shareTitle.addEventListener('click',e=>{
+//     shareSocials.classList.toggle('show') ;
+// })
+
+document.querySelector('#medicInfo').querySelectorAll('.withCollapse').forEach((withCollapse,i,all) => {
+   all = [...all] ;
+   let others = all.filter(elm =>elm!=withCollapse);
+   let font = new FontFaceObserver('iranSans');
+   font.load().then(()=>new Collapse(withCollapse,others));
 })
