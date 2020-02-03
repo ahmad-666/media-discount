6a58e83f8b0dfd6f35b9c7375de863880628a489
ahmad-666 <ahmadrezagghz666@gmail.com>
2020-02-02 16:26:46 -0800

    before re resp info

--- src/articles/articles.js
+++ src/articles/articles.js
@@ -1,7 +1,8 @@
 //load styles
 import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
 import '../framework.scss' ;
-// import '../utilities/styles/form.scss'
+import '../../node_modules/select2/dist/css/select2.min.css' ;
+import '../utilities/styles/form.scss'
 import '../init.scss' ;
 import '../index/styles/header.scss' ;
 import '../index/styles/menu.scss' ;
@@ -11,6 +12,7 @@ import '../articles/styles/articles.scss' ;
 import '../index/styles/footer.scss' ;
 //load scripts
 import '../index/scripts/topMenu.js' ;
+import '../index/scripts/search.js' ;
 import '../index/scripts/menu.js' ;
 import '../index/scripts/smooth.js' ;
 //load videos
